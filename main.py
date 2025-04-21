977c5f0aba87b9e2843329b741e4484d7fc0e2e0
Your Name <you@example.com>
2025-04-21 21:04:50 +0530

    Fixed /dub route to return audio player

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
+from flask import Flask, request, send_from_directory, render_template_string
+
+app = Flask(__name__)
+
 @app.route('/dub')
 def dub():
-    youtube_url = request.args.get('url')
-    target_lang = request.args.get('lang')
+    youtube_url = request.args.get("url")
+    lang = request.args.get("lang")
 
-    # Logging input for debug
-    print("Received:", youtube_url, "|", target_lang)
-
-    return f"""
-    <h3>YouTube URL: {youtube_url} | Target Language: {target_lang}</h3>
+    return render_template_string("""
+    <h2>YouTube URL: {{ youtube_url }} | Target Language: {{ lang }}</h2>
     <audio controls>
-        <source src="/static/downloads/dubbed.mp3" type="audio/mpeg">
-        Your browser does not support the audio element.
+      <source src="/static/downloads/dubbed.mp3" type="audio/mpeg">
+      Your browser does not support the audio element.
     </audio>
     <br>
     <a href="/static/downloads/dubbed.mp3" download>Download Dubbed Audio</a>
-    """
+    """, youtube_url=youtube_url, lang=lang)
 
