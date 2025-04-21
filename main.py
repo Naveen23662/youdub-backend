1b592f9f21594b9d29c8fff88b2358f1befc3bbf
Your Name <you@example.com>
2025-04-21 21:26:03 +0530

    Fixed /dub route to show audio and download

--- main.py
+++ main.py
@@ -2,18 +2,27 @@ from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
-@app.route('/dub')
-def dub():
+@app.route("/dub")
+def dub_video():
     youtube_url = request.args.get("url")
-    lang = request.args.get("lang")
+    target_language = request.args.get("lang")
 
-    return render_template_string("""
-    <h2>YouTube URL: {{ youtube_url }} | Target Language: {{ lang }}</h2>
-    <audio controls>
-        <source src="/static/downloads/dubbed.mp3" type="audio/mpeg">
-        Your browser does not support the audio element.
-    </audio>
-    <br>
-    <a href="/static/downloads/dubbed.mp3" download>Download Dubbed Audio</a>
-    """, youtube_url=youtube_url, lang=lang)
+    # HTML Template with audio playback
+    html_template = """
+    <!DOCTYPE html>
+    <html>
+    <head><title>YouDub Output</title></head>
+    <body>
+        <h3>YouTube URL: {{ youtube_url }} | Target Language: {{ target_language }}</h3>
+        <audio controls>
+            <source src="/static/downloads/dubbed.mp3" type="audio/mpeg">
+            Your browser does not support the audio element.
+        </audio>
+        <br><br>
+        <a href="/static/downloads/dubbed.mp3" download>Download Dubbed Audio</a>
+    </body>
+    </html>
+    """
+
+    return render_template_string(html_template, youtube_url=youtube_url, target_language=target_language)
 
