f63b6af7307aa3075afee83c3f3c8d11b2269541
Your Name <you@example.com>
2025-04-21 21:27:51 +0530

    Final fix for audio player and download link

--- main.py
+++ main.py
@@ -7,22 +7,21 @@ def dub_video():
     youtube_url = request.args.get("url")
     target_language = request.args.get("lang")
 
-    # HTML Template with audio playback
-    html_template = """
+    html = """
     <!DOCTYPE html>
     <html>
-    <head><title>YouDub Output</title></head>
+    <head><title>YouDub Result</title></head>
     <body>
         <h3>YouTube URL: {{ youtube_url }} | Target Language: {{ target_language }}</h3>
         <audio controls>
             <source src="/static/downloads/dubbed.mp3" type="audio/mpeg">
-            Your browser does not support the audio element.
+            Your browser does not support the audio tag.
         </audio>
-        <br><br>
+        <br>
         <a href="/static/downloads/dubbed.mp3" download>Download Dubbed Audio</a>
     </body>
     </html>
     """
 
-    return render_template_string(html_template, youtube_url=youtube_url, target_language=target_language)
+    return render_template_string(html, youtube_url=youtube_url, target_language=target_language)
 
